01e49003c80aa9668192b576806ec854bd33e5aa
Arnav Daultani <arnavdaultani@gmail.com>
2022-02-21 14:21:32 +0530

    error handlers

--- src/err.rs
+++ src/err.rs
@@ -1,16 +1,16 @@
 use std::process::exit;
 
-pub fn lhs_error() {
+pub fn lhs_error() -> ! {
     println!("Something went wrong parsing LHS");
-    exit(1);
+    exit(1)
 }
 
-pub fn rhs_error() {
+pub fn rhs_error() -> ! {
     println!("Something went wrong parsing RHS");
-    exit(1);
+    exit(1)
 }
 
-pub fn ast_error() {
+pub fn ast_error() -> ! {
     println!("Something went wrong with the parser");
-    exit(1);
+    exit(1)
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,3 @@
-use std::process::exit;
 use crate::utilties::{is_operator, is_digit};
 use crate::err::{ast_error , lhs_error , rhs_error};
 pub enum Op {
@@ -28,14 +27,12 @@ impl AST {
                     Ok(n) => n,
                     Err(_) => {
                         lhs_error();
-                        exit(1);
                     }
                 };
                 let rhs_parsed = match rhs.parse::<i32>() {
                     Ok(n) => n,
                     Err(_) => {
                         rhs_error();
-                        exit(1)
                     }
                 };
                 let ans = lhs_parsed + rhs_parsed;
@@ -47,14 +44,12 @@ impl AST {
                     Ok(n) => n,
                     Err(_) => {
                         lhs_error();
-                        exit(1);
                     }
                 };
                 let rhs_parsed = match rhs.parse::<i32>() {
                     Ok(n) => n,
                     Err(_) => {
                         rhs_error();
-                        exit(1)
                     }
                 };
                 let ans = lhs_parsed - rhs_parsed;
@@ -66,14 +61,12 @@ impl AST {
                     Ok(n) => n,
                     Err(_) => {
                         lhs_error();
-                        exit(1);
                     }
                 };
                 let rhs_parsed = match rhs.parse::<i32>() {
                     Ok(n) => n,
                     Err(_) => {
                         rhs_error();
-                        exit(1)
                     }
                 };
                 let ans = lhs_parsed * rhs_parsed;
@@ -84,14 +77,12 @@ impl AST {
                     Ok(n) => n,
                     Err(_) => {
                         lhs_error();
-                        exit(1);
                     }
                 };
                 let rhs_parsed = match rhs.parse::<i32>() {
                     Ok(n) => n,
                     Err(_) => {
                         rhs_error();
-                        exit(1)
                     }
                 };
                 let ans = lhs_parsed / rhs_parsed;
@@ -105,92 +96,67 @@ impl AST {
 
 
 pub fn parse(tokens : Vec<String>) {
-    for (index , token) in tokens.iter().enumerate() {
+    for (index , mut token) in tokens.iter().enumerate() {
         if is_operator(&token) {
             match token.as_str() {
                 "+" => {
-                    let rhs = tokens.get(index + 1);
-                    let lhs = tokens.get(index - 1);
-                    
-                    let rhs_parsed = match rhs {
-                        Some(r) => r,
-                        None => {
-                            rhs_error();
-                            exit(1)
-                        }
+
+                    //let rhs = tokens.get(index);
+                    //let lhs = tokens.get(index);
+
+                    let rhs = match tokens.get(index + 1) {
+                        Some(v) => v,
+                        None => rhs_error()
                     };
 
-                    let lhs_parsed = match lhs {
-                        Some(r) => r,
-                        None => {
-                            lhs_error();
-                            exit(1)
-                        }
+                    let lhs = match tokens.get(index.checked_sub(1).unwrap_or(0)) {
+                        Some(v) => v,
+                        None => lhs_error()
                     };
-                    AST::eval(&AST::InfixOp {op : Op::PLUS , rhs :  rhs_parsed.clone() , lhs : lhs_parsed.clone()})
+                    
+                    AST::eval(&AST::InfixOp {op : Op::PLUS , rhs :  rhs.clone() , lhs : lhs.clone()})
                 }
                 "-" => {
-                    let rhs = tokens.get(index + 1);
-                    let lhs = tokens.get(index - 1);
-                    
-                    let rhs_parsed = match rhs {
-                        Some(r) => r,
-                        None => {
-                            rhs_error();
-                            exit(1)
-                        }
+
+                    let rhs = match tokens.get(index + 1) {
+                        Some(v) => v,
+                        None => rhs_error()
                     };
 
-                    let lhs_parsed = match lhs {
-                        Some(r) => r,
-                        None => {
-                            lhs_error();
-                            exit(1)
-                        }
+                    let lhs = match tokens.get(index.checked_sub(1).unwrap_or(0)) {
+                        Some(v) => v,
+                        None => lhs_error()
                     };
-                    AST::eval(&AST::InfixOp {op : Op::MINUS , rhs :  rhs_parsed.clone() , lhs : lhs_parsed.clone()})
+                    
+                    AST::eval(&AST::InfixOp {op : Op::MINUS , rhs :  rhs.clone() , lhs : lhs.clone()})
                 }
                 "/" => {
-                    let rhs = tokens.get(index + 1);
-                    let lhs = tokens.get(index - 1);
                     
-                    let rhs_parsed = match rhs {
-                        Some(r) => r,
-                        None => {
-                            rhs_error();
-                            exit(1)
-                        }
+                    let rhs = match tokens.get(index + 1) {
+                        Some(v) => v,
+                        None => rhs_error()
                     };
 
-                    let lhs_parsed = match lhs {
-                        Some(r) => r,
-                        None => {
-                            lhs_error();
-                            exit(1)
-                        }
+                    let lhs = match tokens.get(index.checked_sub(1).unwrap_or(0)) {
+                        Some(v) => v,
+                        None => lhs_error()
                     };
-                    AST::eval(&AST::InfixOp {op : Op::DIVIDE , rhs :  rhs_parsed.clone() , lhs : lhs_parsed.clone()})
+
+                    AST::eval(&AST::InfixOp {op : Op::DIVIDE , rhs :  rhs.clone() , lhs : lhs.clone()})
                 }
                 "*" => {
-                    let rhs = tokens.get(index + 1);
-                    let lhs = tokens.get(index - 1);
                     
-                    let rhs_parsed = match rhs {
-                        Some(r) => r,
-                        None => {
-                            rhs_error();
-                            exit(1)
-                        }
+                    let rhs = match tokens.get(index + 1) {
+                        Some(v) => v,
+                        None => rhs_error()
                     };
 
-                    let lhs_parsed = match lhs {
-                        Some(r) => r,
-                        None => {
-                            lhs_error();
-                            exit(1)
-                        }
+                    let lhs = match tokens.get(index.checked_sub(1).unwrap_or(0)) {
+                        Some(v) => v,
+                        None => lhs_error()
                     };
-                    AST::eval(&AST::InfixOp {op : Op::MULTIPLY , rhs :  rhs_parsed.clone() , lhs : lhs_parsed.clone()})
+
+                    AST::eval(&AST::InfixOp {op : Op::MULTIPLY , rhs :  rhs.clone() , lhs : lhs.clone()})
                 }
                 "," => AST::eval(&AST::InfixOp {op : Op::COMMA , rhs :  tokens[index + 1].clone() , lhs : tokens[index - 1].clone()}),
                 "(" => AST::eval(&AST::InfixOp {op : Op::LPAREN , rhs :  tokens[index + 1].clone() , lhs : tokens[index - 1].clone()}),
